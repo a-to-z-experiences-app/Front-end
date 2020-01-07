5bb9a592cf52324deea2ded25ac194f5d600a0fb
rodrigograca31 <rodrigograca1993@gmail.com>
2020-01-07 19:35:51 +0000

    🎨 Better looking Experiences listing

--- src/components/experiences/list.jsx
+++ src/components/experiences/list.jsx
@@ -3,21 +3,91 @@ import { useEffect } from "react";
 import { loadExperiences } from "../../state/actionCreators";
 import { connect } from "react-redux";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+import DeleteIcon from "@material-ui/icons/Delete";
+
+const useStyles = makeStyles({
+	card: {
+		maxWidth: 345
+	},
+	media: {
+		height: 140
+	}
+});
+
 const Experiences = ({ experiences, loadExperiences }) => {
 	useEffect(() => {
 		loadExperiences();
 	}, []);
 
+	const classes = useStyles();
+
 	return (
 		<>
-			<h1>Experiences: </h1>
-			{experiences.map(experience => (
-				<div style={{ border: "2px solid black" }}>
-					{experience.title}
-					<br />
-					{experience.description}
-				</div>
-			))}
+			<h1 style={{ fontWeight: "bold", color: "#555" }}>
+				One-of-a-kind activities <br /> hosted by locals:{" "}
+			</h1>
+			<Grid container spacing={2}>
+				{experiences.map(experience => (
+					<Grid item xs={2}>
+						<Card className={classes.card}>
+							<CardActionArea>
+								<CardMedia
+									className={classes.media}
+									image={
+										"https://picsum.photos/seed/" +
+										Math.floor(Math.random() * 100) +
+										"/200/300"
+									}
+									title="Contemplative Reptile"
+								/>
+								<CardContent>
+									<Typography
+										gutterBottom
+										variant="h5"
+										component="h2"
+									>
+										{experience.title}
+									</Typography>
+									<Typography
+										variant="body2"
+										color="textSecondary"
+										component="p"
+									>
+										{experience.description}
+									</Typography>
+								</CardContent>
+							</CardActionArea>
+							<CardActions>
+								{/* <Button size="small" color="primary">
+									Share
+								</Button> */}
+								<Button size="small" color="primary">
+									Learn More
+								</Button>
+								<Button
+									size="small"
+									color="primary"
+									style={{ marginLeft: "auto" }}
+								>
+									<DeleteIcon />
+								</Button>
+							</CardActions>
+						</Card>
+					</Grid>
+				))}
+			</Grid>
 		</>
 	);
 };
